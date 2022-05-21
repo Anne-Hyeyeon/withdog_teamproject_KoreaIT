7a52341831007e70113cae989a46fa61716d8ce0
petahseo <94450217+petahseo@users.noreply.github.com>
2022-05-21 11:26:46 +0900

    setup test home page

--- src/modules/views/hw/Test.js
+++ src/modules/views/hw/Test.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import TestHome from "./TestHome";
+import TestQuestion from "./TestQuestion";
+import TestResult from "./TestResult";
+
 
 function Test(props) {
     return (
-        <div>
-            <header>강아지 MBTI</header>
-            <contents>
-                <title></title>
-            </contents>
+        <div className="Test">
+            <Routes>
+                <Route path='/' element={<TestHome />} />
+                <Route path='/questions' element={<TestQuestion />} />
+                <Route path='/result' element={<TestResult />} />
+            </Routes>
         </div>
     );
 }
 
 export default Test;
+
+
+
---
+++ src/modules/views/hw/TestQuestion.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Question() {
+  return (
+    <div>
+      Questions
+    </div>
+  );
+}
+
+export default Question;
