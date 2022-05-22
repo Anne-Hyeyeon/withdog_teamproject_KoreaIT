10cc5ee4ef06f0dfa04ad6194247edf0aa7d528d
petahseo <94450217+petahseo@users.noreply.github.com>
2022-05-23 01:56:55 +0900

    Test Question
    made question data

--- src/modules/views/hw/pages/TestQuestion.js
+++ src/modules/views/hw/pages/TestQuestion.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Box, Button, Container, LinearProgress, Stack } from '@mui/material';
+import { QuestionData } from '../asset/data/questionData';
 
 const Question = () => {
-
-
   return (
-    <div>
-      Questions
-    </div>
+    <Container fixed>
+      <Box style={{ textAlign: 'center' }}>
+        <LinearProgress color="success" style={{ marginTop: '20px' }} />
+        <Box sx={{ fontSize: '50px' }} >{QuestionData[0].title}</Box>
+        <Stack direction="row" spacing={2} justifyContent="center" >
+          <Button variant='contained' size='large'>{QuestionData[0].answera}</Button>
+          <Button variant='contained' size='large'>{QuestionData[0].answerb}</Button>
+        </Stack>
+      </Box>
+    </Container>
   );
 }
 
