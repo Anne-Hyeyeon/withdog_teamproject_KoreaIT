b1fc446804e2da89231e1bf3557a624e3a64f4d3
thgml00 <jujusohee@gmail.com>
2022-05-23 21:19:56 +0900

    카카오 맵 ver_1

--- src/modules/views/sh/Info.js
+++ src/modules/views/sh/Info.js
@@ -1,16 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { authService, dbService } from '../../../fbase';
+import React, { useState } from 'react';
+import { authService } from '../../../fbase';
 import { useNavigate } from 'react-router-dom';
 import { useAuthState } from "react-firebase-hooks/auth";
-import KakaoMapScript from './KakaoMapScript';
-
-const { kakao } = window;
+import MapContainer from './MapContainer';
 
 const Info = ({refreshUser,userObj}) => {
   /* 사용자 정보 가져오기 */
   const [user] = useAuthState(authService);
   const navigate = useNavigate();
-  const [Place, setPlace] = useState("");
   const [newDisplayName, setNewDisplayName]=useState(userObj.displayName);
   const [newRegion, setNewRegion]=useState(userObj.photoURL);
   const onLogOutClick=()=>{
@@ -36,14 +33,8 @@ const Info = ({refreshUser,userObj}) => {
       })
       refreshUser()
     }
-    setPlace(user.photoURL);
   }
-  useEffect(() => {
-    KakaoMapScript();
-  }, []);
-
   console.log(user);
-
 
   return (
     <>
@@ -53,16 +44,9 @@ const Info = ({refreshUser,userObj}) => {
         <input type="text" placeholder="Region Name" value={newRegion} onChange={onChangeRegion}/>
         <input type="submit" value='Update Profile' />
       </form>
+      <MapContainer searchPlace={user.photoURL+"공원"} />
       <h1>{user.displayName}</h1>
       <h1>{user.photoURL}</h1>
-      {/* <form className="inputForm" onSubmit={onSubmit}>
-        <input placeholder="검색어를 입력하세요" onChange={onChange} value={InputText} />
-        <button type="submit">검색</button>
-      </form> */}
-      <div id='myMap' style={{
-        width: '500px',
-        height: '500px'
-      }}></div>
       <button onClick={onLogOutClick}>Log out</button>
     </>
   );
