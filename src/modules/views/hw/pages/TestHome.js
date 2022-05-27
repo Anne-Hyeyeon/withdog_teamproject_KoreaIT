5a44c94b97796d714925832c6c08959e3e7257a2
petahseo <94450217+petahseo@users.noreply.github.com>
2022-05-27 20:05:41 +0900

    MUI

--- src/modules/views/hw/pages/TestHome.js
+++ src/modules/views/hw/pages/TestHome.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, Container, Typography } from '@mui/material';
+import { Box, Button, Card, Container, Grid, Typography } from '@mui/material';
 
 //import { useNavigate } from 'react-router-dom'
 
@@ -11,8 +11,28 @@ const Test = () => {
   //   navigate('/testquestion')
   // }
   return (
-    <Container component="main" maxWidth='md'>
-      <Box component="div"
+    <Container maxWidth='xl' sx={{ mt: 15 }}>
+      <Typography variant="h4" marked="center" align="center" component="h2" >
+        Dog MBTI
+      </Typography>
+      <Typography align="center" sx={{ mt: 3 }}>
+        나는 어떤 강아지 일까?
+      </Typography>
+      <Typography align="center" sx={{ mt: 1 }}>
+        MBTI를 기반으로 하는 "내가 강아지라면?!"
+      </Typography>
+
+      <Grid container>
+        <Grid item maxWidth='sm'>
+          <Card variant="outlined" sx={{ maxWidth: '100%', maxHeight: '100%', borderRadius: 3, mb: 3 }}>
+            asfasdf
+          </Card>
+        </Grid>
+      </Grid>
+
+
+
+      <Box maxWidth='sm'
         sx={{
           backgroundImage: `url(${background})`,
           backgroundPosition: 'center',
@@ -20,13 +40,14 @@ const Test = () => {
           backgroundSize: 'cover',
           //position: 'relative',
           //display: 'flex',
-          //justifyContent: 'center',
+          justifyContent: 'center',
           alignItems: 'center',
           //color: '#fff',
           height: '80vh',
           width: '100%',
           minHeight: 500,
           maxHeight: 1300,
+          mt: 6
         }
         } >
 
