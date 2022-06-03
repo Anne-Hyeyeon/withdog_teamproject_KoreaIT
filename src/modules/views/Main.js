2f76ec079dac2e5b4aeb89c43a872eb2fa218810
Anne-Hyeyeon <khy2106@naver.com>
2022-06-03 13:30:21 +0900

    메타태그 수정

--- src/modules/views/Main.js
+++ src/modules/views/Main.js
@@ -5,10 +5,19 @@ import ProductCategories from './ProductCategories';
 import ProductCTA from './ProductCTA';
 import SponsoredBanner from './SponsoredBanner';
 import Banner from './Banner'
+import Helmet from 'react-helmet'
 
 function Main({ userObj } ) {
-    return (
+    return (       
         <React.Fragment>
+            <Helmet>
+                <title>윗독(withdog)</title>
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://withdog0603.netlify.app/" />
+                <meta property="og:title" content="윗독(WithDog)" />
+                <meta property="og:description" content="견주들의 놀이터" />
+                <meta property="og:image" content="src/modules/views/hy/assets/withdogmeta.png" />
+            </Helmet>
             { userObj ? <Banner /> : <ProductHero />
             }
             <ProductValues />
