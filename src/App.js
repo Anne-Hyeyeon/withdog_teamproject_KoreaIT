1271ee39c20fd542c347eeb0fa4babdc3eaf1ae8
petahseo <94450217+petahseo@users.noreply.github.com>
2022-06-07 21:40:45 +0900

    add scroll button

--- src/App.js
+++ src/App.js
@@ -1,45 +1,47 @@
 import * as React from 'react';
 import withRoot from './modules/withRoot';
 import MainRouter from './MainRouter'
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import { authService } from './fbase';
 import AppAppBar from './modules/views/pages/main/AppAppBar';
 import AppFooter from './modules/views/pages/main/AppFooter';
+import ScrollButton from './modules/components/ScrollButton';
 
 function App() {
-  const [isLoggedIn,setIsLoggedIn]=useState(false)
-  const [userObj, setUserObj] =useState("")
-  useEffect(()=>{
-    authService.onAuthStateChanged((user)=> {
-      if(user){
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [userObj, setUserObj] = useState("")
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
         setIsLoggedIn(true)
         setUserObj({
-          displayName:user.displayName,
-          uid:user.uid,
-          photoURL:user.photoURL,
-          updateProfile:(args)=> user.updateProfile(args)
+          displayName: user.displayName,
+          uid: user.uid,
+          photoURL: user.photoURL,
+          updateProfile: (args) => user.updateProfile(args)
         });
-      } else{
+      } else {
         setIsLoggedIn(false)
       }
     })
   }, []);
-  const refreshUser=()=>{
-    const user=authService.currentUser;
+  const refreshUser = () => {
+    const user = authService.currentUser;
     setUserObj({ // user에서 값을 세분화 시켜서 분리해서 사용
-      displayName:user.displayName,
-      uid:user.uid,
-      photoURL:user.photoURL,
-      updateProfile:(args)=> user.updateProfile(args)
+      displayName: user.displayName,
+      uid: user.uid,
+      photoURL: user.photoURL,
+      updateProfile: (args) => user.updateProfile(args)
     });
     console.log(authService.currentUser)
   }
   return (
     <>
       <AppAppBar userObj={userObj} />
-      <MainRouter refreshUser={refreshUser} isLoggedIn={isLoggedIn} userObj={userObj}/>
+      <MainRouter refreshUser={refreshUser} isLoggedIn={isLoggedIn} userObj={userObj} />
       <AppFooter />
-    </> 
+      <ScrollButton />
+    </>
   );
 }
 
