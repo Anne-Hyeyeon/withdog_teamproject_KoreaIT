235f095a014933fd0df61c94995e34083a0e0e44
Hyeyeon(Anne) <khy2106@naver.com>
2022-09-22 20:56:26 +0900

    주석 삭제

--- src/modules/withRoot.js
+++ src/modules/withRoot.js
@@ -7,7 +7,6 @@ export default function withRoot(Component) {
   function WithRoot(props) {
     return (
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...props} />
       </ThemeProvider>
